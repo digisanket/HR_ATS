40811667182f08a751fdb410047f7f58bebc0fc0
digisanket <sanket@digitalflake.com>
2022-09-09 15:31:32 +0530

    added the Excel sheet

--- application/views/candidate/bulk_import.php
+++ application/views/candidate/bulk_import.php
@@ -36,7 +36,12 @@
             <div class="col-md-12">
                 <div class="box box-primary">
                     <div class="box-body">
-
+                        <div class="row">
+                            <div class="col-6">
+                                <label class="form-label">Upload Invoice Copy</label>
+                                <input type="file" id="uplaodimage" name="uplaodimage" class="form-control" aria-label="file example" accept="application/pdf" >
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
